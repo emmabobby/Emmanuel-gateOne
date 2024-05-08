7eb327f64fce8065b5f894186a6aff91133a4123
emmabobby <evansjota@gmail.com>
2024-05-08 13:31:03 +0100

    day 3

---
+++ GATEONE DAY 3/JAVA/TaskOne.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class TaskOne{
+	public static void main(String...args){
+
+		Scanner scanner = new scanner(System.in);
+		total score = 0;
+		student score = 0
+	        attempts = 10;
+
+		for(int attempt = 0; attemt < 10; attempt++){
+			int number 1 = random.nextInt(10) + 10;
+			int number 2 = random.nextInt(10) + 10;
+			System.out.print("Enter answer");
+		
+				if(if answer == correctAnswer){
+					System.out.println("correct");
+				else{
+					System.out.println("incorrect. Try again");
+				}
+					
+		}
+			total score += student score;
+			System.out.print("your final score is: " + studentScore):
+			System.out.print("Total score of all users: " + totalScore);
+	
+}
+
+
+
+}
