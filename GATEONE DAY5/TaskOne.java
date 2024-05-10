0187e1ad0b6dc33e26485fc82cb9fc4167a1bd70
emmabobby <evansjota@gmail.com>
2024-05-10 13:06:21 +0100

    day 5

---
+++ GATEONE DAY5/TaskOne.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class TaskOne{
+	public static void main(String...args){
+
+		string creditCardNumber = "12345667890123456";
+		boolean isValid = checkCreditCardNumber;
+
+		if (isValid) {
+		    System.out.println("The credit card is valid. ");
+		}else {
+		    System.out.println("The credit card is not valid. ");
+		}
+	}
+	
+	public static boolean validCardNumber(String creditCardNumber){
+		String creditCardNumber = ("-" + "");
+
+		if ( cardNumber.length() ==16){
+			System.out.println("CreditCardNumber is 16 digits.");
+
+	return false;
+
+		}
+
+		if (cardNumber !=4 && cardNumber != 5 && cardNumber != 6) {
+
+	return false;
+
+		}
+		
+		if ("creditCardNumber is not a digit"){
+			
+	return false;
+
+		}
+		if ("creditCardNumber is valid" ){
+	 return true;
+
+	}
