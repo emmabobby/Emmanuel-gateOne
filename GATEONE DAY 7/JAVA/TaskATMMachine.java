179b7856bdd6cb19f34a0d317942233feedd902e
emmabobby <evansjota@gmail.com>
2024-05-13 16:53:43 +0100

    GateOne Day 7 update

--- GATEONE DAY 7/JAVA/TaskATMMachine.java
+++ GATEONE DAY 7/JAVA/TaskATMMachine.java
@@ -2,13 +2,33 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class TaskATMMachine {
+
+	public static ArrayList<String>Machine = new ArrayList<>();
+
+
 	public static void main(String...args){
 
 		Scanner scanner = new Scanner(System.in);
 
-		public static ArrayList<
+		int balance = 1000;
+		int withdraw,deposit;
+
+	
+			while(true) {
+				System.out.println("Welcome to Banke bank ATM");
+				System.out.pritln("Create account");
+				System.out.pritln("Close account");
+				System.out.pritln("Deposit account");
+				System.out.pritln("Withdraw money");
+				System.out.pritln("Check account balance");
+				System.out.pritln("Transfer money");
+				System.out.pritln("Change pin");
+
+
+			}
 	}
 
 
 }
 
+5
