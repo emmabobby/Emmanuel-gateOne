fa0d17e468d1f9a9cd2fd06efe29cf54b6d29eeb
emmabobby <evansjota@gmail.com>
2024-05-14 00:02:13 +0100

    GateOne Day 7 Update

--- GATEONE DAY 7/JAVA/TaskATMMachine.java
+++ GATEONE DAY 7/JAVA/TaskATMMachine.java
@@ -7,10 +7,10 @@ private String firstName;
 private String lastName;
 private String pin;
 private BigDecimal balance;
-private string number;
+private String number;
 
 
-public  Account (String firstName,String lastName,String pin, String number){
+public TaskATMMachine(String firstName,String lastName,String pin, String number){
 this.firstName = firstName;
 this.lastName = lastName;
 this.pin = pin;
@@ -23,5 +23,60 @@ if(pin.equals(this.pin)){
 result = balance;
 }
 return result;
+
+}
+
+public void deposit(BigDecimal amount){
+validateAmount(amount);
+if(balance == null){balance = BigDecimal.ZERO;}
+balance = balance.add(amount);
+
+
+}
+public void validateAmount(BigDecimal amount){
+	if (amount.compareTo(BigDecimal.ZERO) <= 0){
+		System.out.println("amount can not be less than 0");
+		
+	}
+}
+
+public void withdraw(BigDecimal amount,String pin){
+if(!(pin.equals(this.pin))){
+	System.out.println("Incorrect pin");
+}
+
+validateAmount(amount);
+if(balance == null){balance = BigDecimal.ZERO;}
+	if(this.balance.compareTo(amount) < 0){
+		System.out.println("Insufficient fund");
+	}
+	
+		balance = balance.subtract(amount);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
