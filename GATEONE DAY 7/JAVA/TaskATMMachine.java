4ae70bc511694a022fdb1617e2fd1efd911b7166
emmabobby <evansjota@gmail.com>
2024-05-13 15:02:00 +0100

    GateOne day 7

---
+++ GATEONE DAY 7/JAVA/TaskATMMachine.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class TaskATMMachine {
+	public static void main(String...args){
+
+		Scanner scanner = new Scanner(System.in);
+
+		public static ArrayList<
+	}
+
+
+}
+
