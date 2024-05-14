3a64d8c4e82ac5572906dad11d6c316514036949
emmabobby <evansjota@gmail.com>
2024-05-14 16:13:53 +0100

    GateOne Day 7 Update

--- GATEONE DAY 7/JAVA/TaskATMMachine.java
+++ GATEONE DAY 7/JAVA/TaskATMMachine.java
@@ -17,40 +17,61 @@ this.pin = pin;
 this.number = number;
 }
 
-public double checkBalance(String pin){
-double result = 0.0;
-if(pin.equals(this.pin)){
-result = balance;
+public void checkBalance(String pin){
+
+if(!(pin.equals(this.pin))){
+System.out.println("wrong pin");
 }
-return result;
+else{
+System.out.println(balance);
+}
+
 
 }
 
 public void deposit(double amount){
-validateAmount(amount);
-balance = balance + amount;
-
-
-}
-public void validateAmount(double amount){
-	if (amount <= 0.0){
+if (amount <= 0.0){
 		System.out.println("amount can not be less than 0");
+
 		
 	}
+else{
+balance = balance + amount;
+System.out.println("Deposited successfully");
 }
+
+}
+
+	
+
 
 public void withdraw(double amount,String pin){
 if(!(pin.equals(this.pin))){
 	System.out.println("Incorrect pin");
 }
 
-validateAmount(amount);
-	if(this.balance < 0.0){
+else if (amount <= 0.0){
+		System.out.println("amount can not be less than 0");
+
+		
+	}
+	else if(this.balance <= 0.0 || this.balance < amount){
 		System.out.println("Insufficient fund");
 	}
+		else{
+		
 	
 		balance = balance - amount;
+		System.out.println("Withdraw successfully");
+	
+
 }
+}
+public void setPin(String pin){
+this.pin = pin;
+}
+
+public String getPin(){return pin;}
 
 public String getNumber(){return number;}
 
@@ -72,8 +93,11 @@ return account;
 public static void mainMenu(){
 Scanner scanner = new Scanner(System.in);
 System.out.println("""
-press 1 -> Create account;
-
+press 1 -> Create account
+press 3 -> Deposit
+press 4 -> Withdraw
+press 5 -> Check balance
+press 7 -> Change pin
 """);
 String userInput = scanner.nextLine();
 
@@ -82,6 +106,61 @@ case "1":
        createUserAccount();
        break;   
 
+case "3":
+
+   	System.out.println("Enter AccountNumber: ");
+	String accountNumber = scanner.nextLine();
+	System.out.println("Enter Amount: ");
+	double amount = scanner.nextDouble();
+	depositMethod(accountNumber,amount);
+	
+	mainMenu();
+
+
+	break;
+
+case "4":
+	System.out.println("Enter AccountNumber: ");
+	String acctNumber = scanner.nextLine();
+	
+	System.out.println("Enter pin: ");
+	String pin = scanner.nextLine();
+
+	System.out.println("Enter Amount: ");
+	double withdrawAmount = scanner.nextDouble();
+	withdrawMethod(acctNumber,withdrawAmount,pin);
+	
+	mainMenu();
+
+	break;
+
+
+case "5":
+	System.out.println("Enter AccountNumber: ");
+	String accNumber = scanner.nextLine();
+	
+	System.out.println("Enter pin: ");
+	String digitpin = scanner.nextLine();
+	checkBalance(accNumber,digitpin);
+
+	mainMenu();
+
+	break;
+	
+case "7":
+	
+	System.out.println("Enter AccountNumber: ");
+	String accNumber2 = scanner.nextLine();
+	
+	System.out.println("Enter old pin: ");
+	String oldPin = scanner.nextLine();
+
+	System.out.println("Enter newPin: ");
+	String newPin = scanner.nextLine();
+	changePin(accNumber2,oldPin,newPin);
+	mainMenu();
+
+	break;
 
 }}
 
@@ -119,7 +198,8 @@ return result;
 }
 
 
-public static void deposit(String acctNumber,double amount){
+
+public static void depositMethod(String acctNumber,double amount){
 TaskATMMachine output = findAccountNumber(acctNumber);
 
 if(output == null){
@@ -132,11 +212,50 @@ output.deposit(amount);
 }
 
 
+public static void withdrawMethod(String acctNumber,double amount, String pin){
+TaskATMMachine output = findAccountNumber(acctNumber);
+
+if(output == null){
+System.out.println("Account number is incorrect");
+mainMenu(); 
+}
+
+output.withdraw(amount,pin);
+
+}
+
+public static void checkBalance(String accountNumber,String pin){
+TaskATMMachine output = findAccountNumber(accountNumber);
+
+if(output == null){
+System.out.println("Account number is incorrect");
+mainMenu(); 
+}
+
+output.checkBalance(pin);
 
 
 
+}
+public static void changePin(String accountNumber,String oldPin,String newPin){
+TaskATMMachine output = findAccountNumber(accountNumber);
 
+if(output == null){
+System.out.println("Account number is incorrect");
+mainMenu(); 
+}
 
+else if(output.getPin().equals(oldPin)){
+output.setPin(newPin);
+System.out.println("new pin is " + output.getPin());
+
+}
+
+else{
+System.out.println("wrong pin");
+}
+
+}
 
 
 
